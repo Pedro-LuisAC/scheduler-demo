955d5cb1c1bc0735a66e1f5e903d231ad5acadb8
mittles <mittlesbr@gmail.com>
2017-10-01 13:06:50 -0300

    funcionou!

--- src/main.c
+++ src/main.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
 
 int main() {
-  int a[5];
+  int	 a[5];
+  int flag = 0;
   int n;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
+  for (int i=1; i<=(n); i++) {
+     /* Imprime resultado da n-esima iteracao do scheduler */
+    if((i%a[0])==0){
+    	printf("0");
+    }
+    else flag++;
+    if((i%a[1])==0){
+    	printf("1");
+    }
+    else flag++;
+    if((i%a[2])==0){
+    	printf("2");
+    }
+    else flag++;
+    if((i%a[3])==0){
+    	printf("3");
+    }
+    else flag++;
+    if((i%a[4])==0){
+    	printf("4");
+    }
+    else flag++;
+    if (flag == 5){
+    	printf("-");
+    }
+    flag =0;
+    printf("\n");
   }
 
   return 0;
